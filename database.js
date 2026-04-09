dd423f105427fdb93e90e5a9f12169c71e17d792
Morita30 <miltondulantoostos@gmail.com>
2026-04-09 02:38:27 -0500

    Actualizar solo se cambio de 54 a 20 los productos demo.js

--- database.js
+++ database.js
@@ -11,8 +11,8 @@ const productosData = [
     { c: "embrague", m: "nissan", n: "Kit Embrague NP300", p: 850 }
 ];
 
-// Relleno automático para pruebas (hasta 54 items)
-while(productosData.length < 54) {
+// Relleno automático para pruebas (hasta 20 items)
+while(productosData.length < 20) {
     let base = productosData[Math.floor(Math.random() * productosData.length)];
     productosData.push({...base, n: base.n + " " + productosData.length});
 }
